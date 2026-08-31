6c114946ce48b9f190d7a98ce37d3020f19fcbd5
vinay soni <vsoni6588@gmail.com>
2026-08-31 18:46:07 +0530

    Fix casing in authMiddleware import

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, LoginUser, getUsers, verifyOtp } = require("../controllers/authcontroller");
-const { protect } = require("../middleware/authMiddleware");
+const { protect } = require("../middleware/authmiddleware");
 const { admin } = require("../middleware/adminMiddleware");
 
 
