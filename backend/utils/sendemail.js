17f878383c2bb9d6ef6e378ec129db929a0c1b58
vinay soni <vsoni6588@gmail.com>
2026-08-31 21:10:01 +0530

    Refactor sendEmail function for SMTP configuration
    Updated email sending function to use SMTP settings.

--- backend/utils/sendemail.js
+++ backend/utils/sendemail.js
@@ -1,24 +1,32 @@
-const nodeMailer = require('nodemailer');      //importing nodemailer module to send email
+const nodeMailer = require('nodemailer');
 
-const sendEmail = async (to, subject, text) => {    //function to send email using nodemailer
-    try {
-        const transporter = nodeMailer.createTransport({         //creating a transporter object to send email
-            service: 'gmail',                               //using gmail as the email service
-            auth: {                                       //authentication for the email service
-                user: process.env.EMAIL_USER,             //email id of the sender
-                pass: process.env.EMAIL_PASS              //password of the sender email id
-            }
-        });
-        const mailOptions = {                           //mail options to be sent to the user
-            from: process.env.EMAIL_USER,
-            to,
-            subject,
-            text
-        };
-        await transporter.sendMail(mailOptions);         //sending the email to the user
-    } catch (error) {
-        console.error('Error sending email:', error);
-        throw error;
-    }
+const sendEmail = async (to, subject, text) => {
+  try {
+    const transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log('Email sent successfully to:', to);
+
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
 };
+
 module.exports = sendEmail;
