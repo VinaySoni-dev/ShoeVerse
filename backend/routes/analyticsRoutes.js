c78368e0bbc01434764d339b511a5ee808dd3be0
vinay soni <vsoni6588@gmail.com>
2026-08-31 18:48:51 +0530

    Fix casing in import for authmiddleware

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authmiddleware');
 const { admin } = require('../middleware/adminMiddleware');
 const { getAdminstats } = require('../controllers/analyticsController');
 
